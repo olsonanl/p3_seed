894f066b72ea2f1b8277ff7f1d355661524cf4be
Robert Olson <olson@mcs.anl.gov>
2016-09-22 15:05:24 -0500

    Update to 1.002

--- lib/myRASTVersion.pm
+++ lib/myRASTVersion.pm
@@ -6,9 +6,9 @@ sub new
 {
     my($class) = @_;
     my $self = {
-	release => "1.001",
-	package_date => 1474574304,
-	package_date_str => "Sep 22, 2016 14:58:24",
+	release => "1.002",
+	package_date => 1474574697,
+	package_date_str => "Sep 22, 2016 15:04:57",
     };
     return bless $self, $class;
 }
